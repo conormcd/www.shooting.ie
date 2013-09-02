e29a801537e71d86ccca269d1456e7ee612f80d8
Conor McDermottroe <conor@mcdermottroe.com>
2013-09-02 20:15:48 +0100

    Add the NRAI calendar.

--- feed/calendars/Calendars.php
+++ feed/calendars/Calendars.php
@@ -14,7 +14,7 @@ extends Feed
     const CRUFT = '/\s*\*?\s*(?:postponed|cancell?ed|confirmed)\s*\*?\s*$/i';
 
     /**
-     * Initialise with a directory which will contain a .json file for each 
+     * Initialise with a directory which will contain a .json file for each
      * calendar.
      *
      * @param string $data_dir The path to the directory.
@@ -40,6 +40,9 @@ extends Feed
                     case 'Google':
                         $cal = $this->readGoogleCalendar($cal);
                         break;
+                    case 'NRAI':
+                        $cal = $this->readNRAICalendar($cal);
+                        break;
                     default:
                         throw new Exception(
                             "Unknown calendar type: {$cal['type']}"
@@ -52,6 +55,11 @@ extends Feed
                 }
             }
 
+            $this->data = $this->removeOutOfRangeEvents(
+                $this->data,
+                time() - 86400,
+                time() + (86400 * 365)
+            );
             $this->data = $this->removeDuplicateEvents($this->data);
             $this->data = $this->removeCancelledEvents($this->data);
 
@@ -82,7 +90,7 @@ extends Feed
     }
 
     /**
-     * Generate the set of unique pairs of numbers starting at $min and up to 
+     * Generate the set of unique pairs of numbers starting at $min and up to
      * but not including $max.
      *
      * @param int $min The inclusive lower bound of the range.
@@ -103,6 +111,26 @@ extends Feed
     }
 
     /**
+     * Remove events that are not inside a specific date range.
+     *
+     * @param array $data  This will be $this->data
+     * @param int   $start The inclusive lower bound of the time range.
+     * @param int   $end   The inclusive lower bound of the time range.
+     *
+     * @return array A copy of $this->data with the out-of-range events
+     *               removed.
+     */
+    public function removeOutOfRangeEvents($data, $start, $end) {
+        $new_data = array();
+        foreach ($data as $timestamp => $events) {
+            if ($timestamp >= $start && $timestamp < $end) {
+                $new_data[$timestamp] = $events;
+            }
+        }
+        return $new_data;
+    }
+
+    /**
      * Remove events that are duplicated across multiple calendars.
      *
      * @param array $data This will be $this->data
@@ -113,7 +141,7 @@ extends Feed
         $new_data = array();
         foreach ($data as $timestamp => $events) {
             $num_events = count($events);
-            
+
             // Detect the similarity
             $similar_pairs = array();
             foreach ($this->uniquePairsInRange(0, $num_events) as $p) {
@@ -166,12 +194,12 @@ extends Feed
     }
 
     /**
-     * Read a Google calendar and augment the provided structure with a list of 
+     * Read a Google calendar and augment the provided structure with a list of
      * events.
      *
      * @param array $cal A calendar structure as decoded from the JSON file.
      *
-     * @return array The passed-in calendar structure with an events list added 
+     * @return array The passed-in calendar structure with an events list added
      *               to it.
      */
     private function readGoogleCalendar($cal) {
@@ -185,22 +213,18 @@ extends Feed
         }
 
         $cal['events'] = array();
-        $cutoff_start = time() - 86400;
-        $cutoff_end = time() + (86400 * 365);
         foreach ($gcal_events as $event) {
             foreach ($this->calculateGoogleTimestamps($event['when']) as $ts) {
-                if ($ts > $cutoff_start && $ts < $cutoff_end) {
-                    $cal['events'][] = array(
-                        'timestamp' => $ts,
-                        'date' => strftime('%Y-%m-%d', $ts),
-                        'title' => $event['title'],
-                        'details' => $event['details'],
-                        'status' => $event['status'],
-                        'location' => $event['location'],
-                        'priority' => $cal['priority'],
-                        'calendar' => $cal['name'],
-                    );
-                }
+                $cal['events'][] = array(
+                    'timestamp' => $ts,
+                    'date' => strftime('%Y-%m-%d', $ts),
+                    'title' => $event['title'],
+                    'details' => $event['details'],
+                    'status' => $event['status'],
+                    'location' => $event['location'],
+                    'priority' => $cal['priority'],
+                    'calendar' => $cal['name'],
+                );
             }
         }
 
@@ -208,7 +232,7 @@ extends Feed
     }
 
     /**
-     * From a Google Calendar "when" expression, calculate a list of 
+     * From a Google Calendar "when" expression, calculate a list of
      * appropriate UNIX timestamps for the start of the event.
      *
      * @param array $when A Google Calendar "when" field.
@@ -225,6 +249,134 @@ extends Feed
             }
         }
         return $timestamps;
+    }
+
+    /**
+     * Read the NRAI calendar.
+     *
+     * @param array $cal A calendar structure as decoded from the JSON file.
+     *
+     * @return array The passed-in calendar structure with an events list added
+     *               to it.
+     */
+    private function readNRAICalendar($cal) {
+        $data = $this->cachedURLFetch($cal['url']);
+
+        // Parse the table out of the source using regular expressions. What
+        // could possibly go wrong here?
+        $data = preg_replace('/^.*<tbody.*?>(.*?)<\/tbody>.*$/s', '$1', $data);
+        $data = preg_replace('/(<[A-Za-z]+)(?:\s+.*?)(>)/s', '$1$2', $data);
+        $data = preg_replace('/<(?!\/?t[dr]).*?>/', '', $data);
+
+        // Chop it up into tokens.
+        $tokens = array_filter(
+            array_map(
+                'trim',
+                preg_split(
+                    '/(<.*?>)/',
+                    $data,
+                    -1,
+                    PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE
+                )
+            )
+        );
+
+        // Transform the data into a 2D array
+        $data = array();
+        $row = 0;
+        $col = 0;
+        foreach ($tokens as $token) {
+            switch ($token) {
+                case '<tr>':
+                case '<td>':
+                    // Do nothing
+                    break;
+                case '</tr>':
+                    $row++;
+                    $col = 0;
+                    break;
+                case '</td>':
+                    $col++;
+                    break;
+                default:
+                    $data[$row][$col] = $token;
+                    break;
+            }
+        }
+        if (!preg_match('/\d+ Calendar/', $data[0][0])) {
+            throw new Exception("Failed to parse NRAI calendar.");
+        }
+
+        $cal['events'] = array();
+        $num_events = count($data) - 1;
+        for ($i = 0; $i < $num_events; $i++) {
+            foreach ($this->calculateNRAITimestamps($data[$i+1][0]) as $ts) {
+                $cal['events'][] = array(
+                    'timestamp' => $ts,
+                    'date' => strftime('%Y-%m-%d', $ts),
+                    'title' => $data[$i+1][1],
+                    'location' => $data[$i+1][3],
+                    'priority' => $cal['priority'],
+                    'calendar' => $cal['name'],
+                );
+            }
+        }
+
+        return $cal;
+    }
+
+    /**
+     * Convert NRAI formatted dates into timestamps.
+     *
+     * @param string $human_dates The date(s) as written on nrai.ie
+     *
+     * @return array An array of UNIX epoch times, one for the start of each
+     *               day in the event.
+     */
+    public function calculateNRAITimestamps($human_dates) {
+        $date_regex = '/^(\d+)(?:st|nd|rd|th)?(?:-(\d+)(?:st|nd|rd|th)?)?\s+(\w+)$/';
+        if (preg_match($date_regex, trim($human_dates), $matches)) {
+            $start = $matches[1];
+            $end = $matches[2] ? ($matches[2] + 1) : ($start + 1);
+            $month = $matches[3];
+            $start = strftime("%Y-%m-%d", strtotime("$start $month"));
+            $end = strftime("%Y-%m-%d", strtotime("$end $month"));
+            return $this->calculateGoogleTimestamps(
+                array(
+                    array(
+                        'start' => $start,
+                        'end' => $end,
+                    )
+                )
+            );
+        } else {
+            throw new Exception("$human_dates is not a valid date expression.");
+        }
+    }
+
+    /**
+     * Fetch a URL and cache the result if possible.
+     *
+     * @param string $url The URL to fetch.
+     *
+     * @return array The contents found at the URL.
+     */
+    private function cachedURLFetch($url) {
+        $contents = null;
+        $key = 'SHOOTING_IE_CALENDAR_' . md5($url);
+        if (function_exists('apc_fetch')) {
+            $contents = apc_fetch($key, $success);
+            if (!$success) {
+                $contents = null;
+            }
+        }
+        if ($contents === null) {
+            $contents = file_get_contents($url);
+            if (function_exists('apc_store')) {
+                apc_store($key, $contents, 3600);
+            }
+        }
+        return $contents;
     }
 
     /**
