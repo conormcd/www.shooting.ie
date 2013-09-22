efad8e7265c832400ed742e7ff224e7d63da5d8b
Conor McDermottroe <conor@mcdermottroe.com>
2013-09-22 11:20:10 +0100

    Tabs -> spaces to match the PHP source.

--- js/map.js
+++ js/map.js
@@ -1,118 +1,118 @@
 function infoAddress(club) {
-	if (club['address']) {
-		address = [];
-		if (club['address']['street-address']) {
-			address.push(club['address']['street-address']);
-		}
-		if (club['address']['extended-address']) {
-			if (typeof club['address']['extended-address'] === 'object') {
-				for (i in club['address']['extended-address']) {
-					address.push(club['address']['extended-address'][i]);
-				}
-			} else {
-				address.push(club['address']['extended-address']);
-			}
-		}
-		if (club['address']['locality']) {
-			if (club['address']['postal-code']) {
-				address.push(club['address']['locality'] + " " + club['address']['postal-code']);
-			} else {
-				address.push(club['address']['locality']);
-			}
-		}
-		return "<br />" + address.join(",<br />") + "<br />";
-	} else {
-		return "";
-	}
+    if (club['address']) {
+        address = [];
+        if (club['address']['street-address']) {
+            address.push(club['address']['street-address']);
+        }
+        if (club['address']['extended-address']) {
+            if (typeof club['address']['extended-address'] === 'object') {
+                for (i in club['address']['extended-address']) {
+                    address.push(club['address']['extended-address'][i]);
+                }
+            } else {
+                address.push(club['address']['extended-address']);
+            }
+        }
+        if (club['address']['locality']) {
+            if (club['address']['postal-code']) {
+                address.push(club['address']['locality'] + " " + club['address']['postal-code']);
+            } else {
+                address.push(club['address']['locality']);
+            }
+        }
+        return "<br />" + address.join(",<br />") + "<br />";
+    } else {
+        return "";
+    }
 }
 
 function infoWindow(club_name, club) {
-	if (club['url']) {
-		info = "<a href=\"" + club['url'] + "\">" + club_name + "</a><br />";
-	} else {
-		info = club_name + "<br />";
-	}
-	info += infoAddress(club);
-	info += "<br />GPS: " + club['latitude'].toFixed(4) + ", " + club['longitude'].toFixed(4);
-	info += "<br /><a target=\"_blank\" ";
-	info += "href=\"http://maps.google.com/maps?saddr=&daddr=" +
-			club['latitude'] + "," +
-			club['longitude'] + "\">Directions...</a>";
-	return info
+    if (club['url']) {
+        info = "<a href=\"" + club['url'] + "\">" + club_name + "</a><br />";
+    } else {
+        info = club_name + "<br />";
+    }
+    info += infoAddress(club);
+    info += "<br />GPS: " + club['latitude'].toFixed(4) + ", " + club['longitude'].toFixed(4);
+    info += "<br /><a target=\"_blank\" ";
+    info += "href=\"http://maps.google.com/maps?saddr=&daddr=" +
+            club['latitude'] + "," +
+            club['longitude'] + "\">Directions...</a>";
+    return info
 }
 
 function drawClubsAndRanges(map, clubs) {
-	for (club_name in clubs) {
-		clubs[club_name]['point'] = new google.maps.LatLng(
-			clubs[club_name]['latitude'],
-			clubs[club_name]['longitude']
-		);
-		clubs[club_name]['marker'] = new google.maps.Marker({
-			position: clubs[club_name]['point'],
-			map: map,
-			title: club_name
-		});
+    for (club_name in clubs) {
+        clubs[club_name]['point'] = new google.maps.LatLng(
+            clubs[club_name]['latitude'],
+            clubs[club_name]['longitude']
+        );
+        clubs[club_name]['marker'] = new google.maps.Marker({
+            position: clubs[club_name]['point'],
+            map: map,
+            title: club_name
+        });
 
-		clubs[club_name]['info_window'] = new google.maps.InfoWindow({
-			content: infoWindow(club_name, clubs[club_name])
-		});
+        clubs[club_name]['info_window'] = new google.maps.InfoWindow({
+            content: infoWindow(club_name, clubs[club_name])
+        });
 
-		google.maps.event.addListener(clubs[club_name]['marker'], 'click', function() {
-			map.panTo(clubs[this.title]['point']);
-			for (club_name in clubs) {
-				clubs[club_name]['info_window'].close();
-			}
-			clubs[this.title]['info_window'].open(map, clubs[this.title]['marker']);
+        google.maps.event.addListener(clubs[club_name]['marker'], 'click', function() {
+            map.panTo(clubs[this.title]['point']);
+            for (club_name in clubs) {
+                clubs[club_name]['info_window'].close();
+            }
+            clubs[this.title]['info_window'].open(map, clubs[this.title]['marker']);
 
-			// Change the URL. Use pushstate if possible.
-			if (typeof history.pushstate == 'function') {
-				history.pushstate({}, this.title, '/#' + this.title);
-			} else {
-				document.location.hash = this.title;
-			}
-		});
-	}
+            // Change the URL. Use pushstate if possible.
+            if (typeof history.pushstate == 'function') {
+                history.pushstate({}, this.title, '/#' + this.title);
+            } else {
+                document.location.hash = this.title;
+            }
+        });
+    }
 }
 
 function loadMap() {
-	var map = new google.maps.Map(
-		document.getElementById('map'),
-		{
-			zoom: 7,
-			center: new google.maps.LatLng(53.5, -8.0),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	);
-	$.get(
-		'/feed/clubs_and_ranges/',
-		function (clubs_and_ranges) {
-			drawClubsAndRanges(map, clubs_and_ranges);
-			goToClubInHash = function() {
-				selectedClub = decodeURIComponent(
-					document.location.hash.substring(1).replace(/\+/g, '%20')
-				);
-				if (selectedClub in clubs_and_ranges) {
-					google.maps.event.trigger(
-						clubs_and_ranges[selectedClub]['marker'],
-						'click',
-						{
-							latLng: clubs_and_ranges[selectedClub]['point']
-						}
-					);
-				} else {
-					for (club_name in clubs_and_ranges) {
-						clubs_and_ranges[club_name]['info_window'].close();
-					}
-					map.panTo(new google.maps.LatLng(53.5, -8.0));
-				}
-			}
-			if (document.location.hash) {
-				goToClubInHash();
-			}
-			if ('onhashchange' in window) {
-				window.onhashchange = goToClubInHash;
-			}
-		},
-		'json'
-	);
+    var map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+            zoom: 7,
+            center: new google.maps.LatLng(53.5, -8.0),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+    );
+    $.get(
+        '/feed/clubs_and_ranges/',
+        function (clubs_and_ranges) {
+            drawClubsAndRanges(map, clubs_and_ranges);
+            goToClubInHash = function() {
+                selectedClub = decodeURIComponent(
+                    document.location.hash.substring(1).replace(/\+/g, '%20')
+                );
+                if (selectedClub in clubs_and_ranges) {
+                    google.maps.event.trigger(
+                        clubs_and_ranges[selectedClub]['marker'],
+                        'click',
+                        {
+                            latLng: clubs_and_ranges[selectedClub]['point']
+                        }
+                    );
+                } else {
+                    for (club_name in clubs_and_ranges) {
+                        clubs_and_ranges[club_name]['info_window'].close();
+                    }
+                    map.panTo(new google.maps.LatLng(53.5, -8.0));
+                }
+            }
+            if (document.location.hash) {
+                goToClubInHash();
+            }
+            if ('onhashchange' in window) {
+                window.onhashchange = goToClubInHash;
+            }
+        },
+        'json'
+    );
 }
